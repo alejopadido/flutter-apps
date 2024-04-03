34c975514a2efcba9c127e0938d18b0cea1e41da
Alejandro Parrado <alejopadido13@gmail.com>
2024-04-03 14:51:32 -0500

    Images added

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -51,4 +51,10 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls',
     icon: Icons.car_rental_outlined,
   ),
+  MenuItem(
+    title: 'Get Started Tutorial',
+    subTitle: 'Introduction to the app',
+    link: '/tutorial',
+    icon: Icons.accessibility_rounded,
+  ),
 ];
