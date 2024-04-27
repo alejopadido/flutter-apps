32a9e63c19b20ba364e6970de932d686efcdcb7a
Alejandro Parrado <alejopadido13@gmail.com>
2024-04-27 17:47:01 -0500

    datasource, mapper and repository

--- cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -1,24 +1,47 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie__moviedb.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
-      adult: moviedb.adult,
-      backdropPath: moviedb.backdropPath != ''
-          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-          : 'https://cdn3.iconfinder.com/data/icons/web-development-and-programming-2/64/development_Not_Found-1024.png',
-      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
-      id: moviedb.id,
-      originalLanguage: moviedb.originalLanguage,
-      originalTitle: moviedb.originalTitle,
-      overview: moviedb.overview,
-      popularity: moviedb.popularity,
-      posterPath: (moviedb.posterPath != '')
-          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
-      releaseDate: moviedb.releaseDate,
-      title: moviedb.title,
-      video: moviedb.video,
-      voteAverage: moviedb.voteAverage,
-      voteCount: moviedb.voteCount);
+        adult: moviedb.adult,
+        backdropPath: moviedb.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://cdn3.iconfinder.com/data/icons/web-development-and-programming-2/64/development_Not_Found-1024.png',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+        adult: moviedb.adult,
+        backdropPath: moviedb.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://cdn3.iconfinder.com/data/icons/web-development-and-programming-2/64/development_Not_Found-1024.png',
+        genreIds: moviedb.genres.map((e) => e.name).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'https://cdn3.iconfinder.com/data/icons/web-development-and-programming-2/64/development_Not_Found-1024.png',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
 }
