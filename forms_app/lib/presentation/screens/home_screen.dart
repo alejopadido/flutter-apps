62ded68a1a85e2e411307f76e07665cf9f64ce96
Alejandro Parrado <alejopadido13@gmail.com>
2024-04-28 16:52:51 -0500

    Cubits finished

---
+++ forms_app/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Cubits'),
+            subtitle: const Text('Simple State Manager'),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            onTap: () => context.push('/cubits'),
+          ),
+          ListTile(
+            title: const Text('BLoC'),
+            subtitle: const Text('Robust State Manager'),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            onTap: () => context.push('/bloc'),
+          ),
+        ],
+      ),
+    );
+  }
+}
