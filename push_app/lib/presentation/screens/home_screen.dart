c950fd621bf8d5dc8752f36104e5ef5da95b2108
Alejandro Parrado <alejopadido13@gmail.com>
2024-04-29 16:18:14 -0500

    Initial config

---
+++ push_app/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Permissions'),
+        actions: [
+          IconButton(
+            onPressed: () {}, //TODO ask for notifications permission
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 1,
+      itemBuilder: (context, index) => const ListTile(),
+    );
+  }
+}
